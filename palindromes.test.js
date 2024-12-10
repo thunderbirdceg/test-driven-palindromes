e1b1a977148d4e9519d476d91168beba2e87fb0f
gokulnath panneerselvam <thunderbirdceg@gmail.com>
2024-12-10 05:42:17 +0530

    :tada: First commit

---
+++ palindromes.test.js
@@ -0,0 +1,8 @@
+const palindromes = require('./palindromes')
+
+
+describe("palindromes()",()=>{
+    it("corrrectly identifies 1 word palindromes",()=>{
+        expect(palindromes("mom")).toEqual(['mom'])
+    })
+})
